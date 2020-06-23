777b4d14180e03679b57eb538df955d5e76152ec
Anderson Kramer <46286421+andersonkramer@users.noreply.github.com>
2020-06-22 23:06:02 -0300

    Curso

--- Exercise01.py
+++ Exercise01.py
@@ -2,3 +2,15 @@ a = 25
 b = 20
 soma = ((a*b)+a)/a
 print("A soma dessa expressao é", soma)
+
+print('Hello World')
+
+print('Anderson')
+print('Kramer')
+
+#Isso é um comentario do Programa
+print('Anderson''Kramer')
+
+nome = 'Anderson'
+sobrenome = 'Kramer'
+print(nome+sobrenome)
