18564ee87dfc3dcd097f8d94e109d80a872b73e8
Anderson Kramer <46286421+andersonkramer@users.noreply.github.com>
2020-06-24 00:37:11 -0300

    commit

---
+++ Desafio01.py
@@ -0,0 +1,10 @@
+#Pergunga a idade e responde se você é Adulto
+
+idade = int(input('Qual é a sua idade?'))
+
+type(idade)
+
+if idade > 20:
+    print('Vocé é uma pessoa adulta!')
+else:
+    print('Vocé é um Adolescente!')
---
+++ Exercise06.py
@@ -0,0 +1,28 @@
+#Aula If Else
+comida = 'pizza'
+if comida == 'pizza':
+    print('possui muitas calorias')
+else:
+    print('desconheco calorias')
+
+#Comparando Igualdades com mais de uma probabilidade
+comida = 'pizza'
+if comida == 'pizza':
+    print('possui muitas calorias')
+elif comida == 'abobora':
+    print('muitas calorias')
+elif comida == 'arroz':
+    print('poucas calorias')
+else:
+    print('Desconheco Calorias')
+
+#Comparando diferencas
+comida = 'pizza'
+if comida != 'pizza':
+    print('não é pizza')
+else:
+    print('É Pizza')
+
+numero = 10
+if numero >= 20:
+    print('É menor')
