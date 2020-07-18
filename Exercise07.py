2c7709f3940740878b74e1b21fee74e851787129
Anderson Kramer <46286421+andersonkramer@users.noreply.github.com>
2020-07-18 19:56:31 -0300

    Exercicio7

--- Exercise07.py
+++ Exercise07.py
@@ -1,4 +1,25 @@
 #Exercício 07 Comando FOR
-
 for i in range(5):
     print(i)
+#Comando While e Break com contagem de tempo
+import time
+contador = 0
+while contador < 10:
+    print('agora não ainda é {}'.format(contador))
+    contador = contador + 1
+    if contador == 7:
+        break
+    time.sleep(contador)
+print('agora sim finalizou a contagem com {}'.format(contador))
+
+#exercicio calculando o fatorial de um numero
+
+import time
+numero = int(input('Digite um número'))
+resultado = 1
+while numero > 1:
+    resultado = resultado * numero
+    print('O Resultado Parcial é {}'.format(resultado))
+    numero = numero - 1
+    time.sleep(1)
+print('Resultado total é {}'.format(resultado))
